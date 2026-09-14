0bd6d74f9d8f02aae8270b087e86d620f8b1ee21
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-14 17:40:50 +0100

    add search/author endpoint

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -8,6 +8,7 @@ class SearchController {
     constructor() {
         this.router = Router();
         this.router.get('/title/:query', this.searchTitle.bind(this));
+        this.router.get('/author/:query', this.searchAuthor.bind(this));
     }
 
     async searchTitle(req: Request, res: Response) {
@@ -20,6 +21,27 @@ class SearchController {
         }
         const books = await sql`
             SELECT * FROM Books WHERE Title LIKE ${ '%' + query + '%' };
+        `;
+        if (books.length > 0) {
+            return res.status(200).json(books);
+        } else {
+            return res.status(404).json({
+                error: 'not_found',
+                error_description: 'No books found.',
+            });
+        }
+    }
+
+    async searchAuthor(req: Request, res: Response) {
+        const query = req.params.query;
+        if (typeof query !== 'string' || query.trim() === '') {
+            return res.status(400).json({
+                error: 'invalid_request',
+                error_description: 'query is required and must be a non-empty string.',
+            });
+        }
+        const books = await sql`
+            SELECT * FROM Books WHERE Author LIKE ${ '%' + query + '%' };
         `;
         if (books.length > 0) {
             return res.status(200).json(books);
