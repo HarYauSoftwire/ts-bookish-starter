9bb4000449f777497efe369d4e8959915e19c087
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-15 12:14:52 +0100

    add user search and listing user borrowed books

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,6 +7,8 @@ class UserController {
 
     constructor() {
         this.router = Router();
+        this.router.get('/search/:query', this.searchUser.bind(this));
+        this.router.get('/:userId', this.listUserBorrows.bind(this));
         this.router.post('/', this.createUser.bind(this));
     }
 
@@ -35,6 +37,57 @@ class UserController {
         `;
         return res.status(201).json(user)
 
+    }
+
+    async searchUser(req: Request<{ query: string }>, res: Response) {
+        const query = req.params.query;
+        if (typeof query !== 'string' || query.trim() === '') {
+            return res.status(400).json({
+                error: 'invalid_request',
+                error_description: 'query is required and must be a non-empty string.',
+            });
+        }
+        const users = await sql`
+            SELECT * FROM Users WHERE UserName LIKE ${ '%' + query + '%' };
+        `;
+        if (users.length > 0) {
+            return res.status(200).json(users);
+        } else {
+            return res.status(404).json({
+                error: 'not_found',
+                error_description: 'No users found.',
+            });
+        }
+    }
+
+    async listUserBorrows(req: Request<{ userId: string }>, res: Response) {
+        const { userId } = req.params;
+
+        const parsedUserId = Number(userId);
+        if (!Number.isInteger(parsedUserId)) {
+            return res.status(400).json({
+                error: 'invalid_request',
+                error_description: 'userId is required and must be an integer.',
+            });
+        }
+
+        const userExists = await sql`SELECT 1 FROM Borrows WHERE BorrowId = ${ parsedUserId }`;
+        if (userExists.length === 0) {
+            return res.status(400).json({
+                error: 'invalid_request',
+                error_description: 'userId does not reference an existing user.',
+            });
+        }
+
+        const borrows = await sql`
+            SELECT
+            Borrows.BorrowId as BorrowId, Books.Title as Title, Books.Author as Author, Books.ISBN as ISBN, Borrows.DueDate as DueDate
+            FROM Borrows INNER JOIN Books
+            ON Borrows.BookId = Books.BookId
+            WHERE Borrows.UserId = ${ userId }
+            AND Borrows.Returned = ${ false };
+        `
+        return res.status(200).json(borrows);
     }
 }
 
