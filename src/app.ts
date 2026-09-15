0cc70ea5f3865c95a5015e8370910ec86ddb6230
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-15 16:28:26 +0100

    use passport and passport-jwt to authenticate users

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import 'dotenv/config';
 
+import secret from './secret';
 import healthcheckRoutes from './controllers/healthcheckController';
 import loginRoutes from './controllers/loginController';
 import bookRoutes from './controllers/bookController';
@@ -10,8 +13,21 @@ import borrowRoutes from './controllers/borrowController';
 
 const port = process.env['PORT'] || 3000;
 
+passport.use(new JwtStrategy(
+    {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: secret,
+    },
+    function (jwt_payload, done) {
+        console.log(jwt_payload);
+        const user = jwt_payload.username;
+        return done(null, user);
+    }
+))
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
@@ -21,7 +37,7 @@ app.listen(port, () => {
  */
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/login', loginRoutes);
-app.use('/books', bookRoutes);
-app.use('/users', userRoutes);
-app.use('/search', searchRoutes);
-app.use('/borrow', borrowRoutes);
+app.use('/books', passport.authenticate('jwt', { session: false }), bookRoutes);
+app.use('/users', passport.authenticate('jwt', { session: false }), userRoutes);
+app.use('/search', passport.authenticate('jwt', { session: false }), searchRoutes);
+app.use('/borrow', passport.authenticate('jwt', { session: false }), borrowRoutes);
